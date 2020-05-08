d401eb6df4363e1496e82e24bce0be6c3c997aaf
shanty <capilshanty@gmail.com>
2020-05-08 09:22:30 +0700

    Menghitung volume dan Validasinya

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast.makeText as toastMakeText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        // val textView1=findViewById<TextView>(R.id.textview1)
+        //val button1=findViewById<Button>(R.id.button1)
+
+
+        textview1.setOnClickListener({
+            Toast.makeText(this, "Hai", Toast.LENGTH_SHORT).show()
+        })
+        button1.setOnClickListener {
+            val intent = Intent(this@MainActivity,HitungActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
